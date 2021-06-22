4fa3d357ce00e6cd90471d1606818fddedc3a182
Richard van Dijk <m.k.van.dijk@liacs.leidenuniv.nl>
2021-06-22 21:42:20 +0200

    License legal text inserted in major source files

--- SensorService/src/sensorservice.c
+++ SensorService/src/sensorservice.c
@@ -1,17 +1,35 @@
 //
-// Copyright:
+// Copyright(c) 2021 LiacsProjects
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Author:
+//
+//   Richard M.K. van Dijk
+//   Research sofware engineer
+//   E: m.k.van.dijk@liacs.leidenuniv.nl
 //
 //   Leiden University,
 //   Faculty of Math and Natural Sciences,
 //   Leiden Institute of Advanced Computer Science (LIACS)
 //   Snellius building | Niels Bohrweg 1 | 2333 CA Leiden
 //   The Netherlands
-//
-// Author:
-//
-//   Richard M.K. van Dijk
-//   Research sofware engineer
-//   M: m.k.van.dijk@liacs.leidenuniv.nl
 //
 
 #include <tizen.h>
